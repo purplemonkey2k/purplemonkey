548389617705799a293489c07aec751bf89a5baf
scarletrubi <you@example.com>
2022-08-12 20:57:53 -0300

    Upload de arquivos

---
+++ script.js
@@ -0,0 +1,6 @@
+var menu_icon = document.querySelector(".menu-icon");
+
+menu_icon.addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+
+});
